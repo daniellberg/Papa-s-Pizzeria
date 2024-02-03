e97b761e1ff2e3e5156617c52795a31f2bc7861d
daniellberg <danielbergons@gmail.com>
2024-02-03 19:16:24 -0300

    feat: choose option to filter results

--- app/src/main/java/com/example/appdepizzaria/MainActivity.kt
+++ app/src/main/java/com/example/appdepizzaria/MainActivity.kt
@@ -18,25 +18,95 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btAll.setOnClickListener {
+            clicked = true
+            if(clicked){
+                binding.btAll.setBackgroundResource(R.drawable.bg_button_enabled)
+                binding.btAll.setTextColor(Color.WHITE)
+                binding.btChicken.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btChicken.setTextColor(R.color.dark_gray)
+                binding.btKebab.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btKebab.setTextColor(R.color.dark_gray)
+                binding.btPizza.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btPizza.setTextColor(R.color.dark_gray)
+                binding.btDrinks.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btDrinks.setTextColor(R.color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.VISIBLE
+                binding.txtListTitle.text = "All"
+            }
+        }
+
         binding.btChicken.setOnClickListener {
             clicked = true
             if(clicked){
                 binding.btChicken.setBackgroundResource(R.drawable.bg_button_enabled)
                 binding.btChicken.setTextColor(Color.WHITE)
-                binding.btAll.setBackgroundResource(R.drawable.bg_buton_disabled)
-                binding.btAll.setTextColor(R.color.dark_grey)
-                binding.btKebab.setBackgroundResource(R.drawable.bg_buton_disabled)
-                binding.btKebab.setTextColor(R.color.dark_grey)
-                binding.btPizza.setBackgroundResource(R.drawable.bg_buton_disabled)
-                binding.btPizza.setTextColor(R.color.dark_grey)
-                binding.btDrinks.setBackgroundResource(R.drawable.bg_buton_disabled)
-                binding.btDrinks.setTextColor(R.color.dark_grey)
-                binding.recyclerViewProducts.visibility = View.INVISIBLE
-                binding.txtListTitle.text = "Chicken"
+                binding.btAll.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btAll.setTextColor(R.color.dark_gray)
+                binding.btKebab.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btKebab.setTextColor(R.color.dark_gray)
+                binding.btPizza.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btPizza.setTextColor(R.color.dark_gray)
+                binding.btDrinks.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btDrinks.setTextColor(R.color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.VISIBLE
+                binding.txtListTitle.text = "Popular Chicken"
             }
         }
 
+        binding.btPizza.setOnClickListener {
+            clicked = true
+            if(clicked){
+                binding.btPizza.setBackgroundResource(R.drawable.bg_button_enabled)
+                binding.btPizza.setTextColor(Color.WHITE)
+                binding.btAll.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btAll.setTextColor(R.color.dark_gray)
+                binding.btChicken.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btChicken.setTextColor(R.color.dark_gray)
+                binding.btKebab.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btKebab.setTextColor(R.color.dark_gray)
+                binding.btDrinks.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btDrinks.setTextColor(R.color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.VISIBLE
+                binding.txtListTitle.text = "Popular Pizza"
+            }
+        }
 
+        binding.btKebab.setOnClickListener {
+            clicked = true
+            if(clicked){
+                binding.btKebab.setBackgroundResource(R.drawable.bg_button_enabled)
+                binding.btKebab.setTextColor(Color.WHITE)
+                binding.btPizza.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btPizza.setTextColor(R.color.dark_gray)
+                binding.btAll.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btAll.setTextColor(R.color.dark_gray)
+                binding.btChicken.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btChicken.setTextColor(R.color.dark_gray)
+                binding.btDrinks.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btDrinks.setTextColor(R.color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.INVISIBLE
+                binding.txtListTitle.text = "Popular Kebab"
+            }
+        }
+
+        binding.btDrinks.setOnClickListener {
+            clicked = true
+            if(clicked){
+                binding.btDrinks.setBackgroundResource(R.drawable.bg_button_enabled)
+                binding.btDrinks.setTextColor(Color.WHITE)
+                binding.btKebab.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btKebab.setTextColor(R.color.dark_gray)
+                binding.btPizza.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btPizza.setTextColor(R.color.dark_gray)
+                binding.btAll.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btAll.setTextColor(R.color.dark_gray)
+                binding.btChicken.setBackgroundResource(R.drawable.bg_button_disabled)
+                binding.btChicken.setTextColor(R.color.dark_gray)
+                binding.recyclerViewProducts.visibility = View.INVISIBLE
+                binding.txtListTitle.text = "Popular Drinks"
+            }
+        }
 
 
     }
